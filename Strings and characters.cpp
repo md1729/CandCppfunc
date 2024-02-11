4ba7461ef683e87a289d0d55a09efa1007aef8a6
md1729 <muhammet.dilek@metu.edu.tr>
2024-02-11 15:23:13 +0300

    char and str functions
    We explained and programmed with c and cpp this project.
    I shown char and str functions with c and cpp programming language

---
+++ Strings and characters.cpp
@@ -0,0 +1,11 @@
+//c++ char=character,string=Textual expressions
+#include<iostream>
+using namespace std;
+main(){
+	setlocale(LC_ALL,"English");
+	char c1='A';
+	string text = "World of Informatics";
+	cout<<c1<<endl;
+	cout<<text;
+	
+}
---
+++ strings and characters.c
@@ -0,0 +1,15 @@
+/*
+Single quotes should be used when using "char" c1='A';
+string(textual expression):c does not have a textual expression of type string.
+We use a character string instead of string.
+char k[20]=" Informatics World"
+We use %s when outputting to the screen.
+
+*/
+
+#include<stdio.h>
+main(){
+char c[20]="World of Informatics";
+//printf("%d",c);
+printf("%s",c);	
+}
